ec21e63826ae668e83bb4d082638fbd99f6a62c8
North McCormick <north@checkster.com>
2017-12-04 10:22:48 -0800

    + UMD Fix

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -1,4 +1,5 @@
 (function (root, factory) {
+    if(!root) { var root = window; }
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define('textAngular', ["rangy","rangy/lib/rangy-selectionsaverestore"], function (a0,b1) {
